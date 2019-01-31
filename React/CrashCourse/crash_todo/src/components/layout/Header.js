29edfed19e4f37281c887c809f7981ae43186093
Silvia Valdez <silvia.valdez.e@gmail.com>
2019-01-31 11:18:29 -0700

    ReactCrashCourse: Add About page.

--- React/CrashCourse/crash_todo/src/components/layout/Header.js
+++ React/CrashCourse/crash_todo/src/components/layout/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Header() {
+export default function Header() {
   return (
     <header style={headerStyle}>
       <h1>TodoList</h1>
+      <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about">About</Link>
     </header>
   )
 }
@@ -15,4 +17,7 @@ const headerStyle = {
   padding: '10px'
 }
 
-export default Header;
+const linkStyle = {
+  color: '#fff',
+  textDecoration: 'none'
+}
