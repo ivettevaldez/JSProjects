0814ff24370b8b848ca75d13b005685909f0e565
Ivette Valdez <ivette.valdez.e@gmail.com>
2019-11-28 15:48:52 +0100

    ReactCompleteGuide: "Person" example using Hooks.

--- React/CompleteGuide/react-complete-guide/src/App.js
+++ React/CompleteGuide/react-complete-guide/src/App.js
@@ -1,42 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [personsState, setPersonsState ] = useState({
     persons: [
       { name: 'Ivette', age: 14 },
       { name: 'Robert', age: 25 },
       { name: 'Sylvia', age: 32 },
     ],
     otherState: 'Some other value'
-  };
+  });
 
-  switchNameHandler = () => {
+  const switchNameHandler = () => {
     // console.log('Was clicked!');
     // DON'T DO THIS: this.state.persons[0].name = "Silvia Ivette";
-    this.setState({
+    setPersonsState({
         persons: [
           { name: 'Silvia Ivette', age: 14 },
           { name: 'Robert', age: 25 },
           { name: 'Sylvia', age: 32 },
       ]
     });
-  }
+  };
+  
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <p>This is really working!</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My hobbies: TV</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-      </div>
-    );
-  }
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} >My hobbies: TV</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+    </div>
+  );
 }
 
-export default App;
+export default app;
