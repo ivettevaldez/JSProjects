867f81de88e05be5ff269c944a4c6c8af6ede6c1
Ivette Valdez <ivette.valdez.e@gmail.com>
2019-11-29 13:54:14 +0100

    ReactCompleteGuide: Pass method references between components and add two way binding.

--- React/CompleteGuide/react-complete-guide/src/App.js
+++ React/CompleteGuide/react-complete-guide/src/App.js
@@ -12,26 +12,44 @@ class App extends Component {
     otherState: 'Some other value'
   };
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     this.setState({
         persons: [
-          { name: 'Silvia Ivette', age: 14 },
+          { name: newName, age: 14 },
           { name: 'Robert', age: 25 },
-          { name: 'Sylvia', age: 32 },
+          { name: 'Sylvia Catalina', age: 32 },
       ]
     });
   }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Ivette', age: 14 },
+        { name: event.target.value, age: 25 },
+        { name: 'Sylvia', age: 32 },
+      ]
+    });
+  };
 
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler("Ivette!!!")}>Switch Name</button>
 
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My hobbies: TV</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, "Ivette!")}
+          changed={this.nameChangedHandler} >My hobbies: TV</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
       </div>
     );
   }
