d91747ea86233060add22d3e05bf4bf2221eb6e0
Ivette Valdez <ivette.valdez.e@gmail.com>
2019-12-04 12:08:37 +0100

    ReactCompleteGuide: Remove styled-components from App.js.

--- React/CompleteGuide/react-complete-guide/src/App.js
+++ React/CompleteGuide/react-complete-guide/src/App.js
@@ -1,22 +1,7 @@
 import React, {Component} from 'react';
-import styled from 'styled-components';
 
 import './App.css';
 import Person from './Person/Person';
-
-const StyledButton = styled.button`
-  background-color: ${props => props.show ? 'red' : 'green'};
-  color: white;
-  font: inherit;
-  border: 1px solid black;
-  padding: 8px;
-  cursor: pointer;
-  
-  &:hover {
-    background-color: ${props => props.show ? 'salmon' : 'lightgreen'};
-    color: black;
-  }
-`;
 
 class App extends Component {
   state = {
@@ -96,11 +81,12 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
-        <StyledButton 
+        <button 
+          className="button"
           onClick={this.togglePeopleHandler}
           show={this.state.showPeople} >
             Toggle People
-        </StyledButton>
+        </button>
         
         { people }
       </div>
