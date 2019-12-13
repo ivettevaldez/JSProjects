5f1eac00a0a1104cd432357e6749a1223e034c33
Ivette Valdez <ivette.valdez.e@gmail.com>
2019-12-13 12:48:04 +0100

    ReactCompleteGuide: Implemented the Backdrop component.

--- React/CompleteGuide/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ React/CompleteGuide/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -94,6 +94,12 @@ class BurgerBuilder extends Component {
         });
     }
 
+    purchaseCancelHandler = () => {
+        this.setState({
+            purchasing: false
+        });
+    }
+
     render () {
         const disabledInfo = {
             ...this.state.ingredients
@@ -105,7 +111,9 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
-                <Modal show={this.state.purchasing} >
+                <Modal 
+                    show={this.state.purchasing}
+                    modalClosed={this.purchaseCancelHandler} >
                     <OrderSummary ingredients={this.state.ingredients} />
                 </Modal>
                 <Burger 
