51ee79cc1c73ad77455d1ef8a89cc07ecb79382f
Ivette Valdez <ivette.valdez.e@gmail.com>
2019-12-16 14:36:33 +0100

    ReactCompleteGuide: Reusing the Backdrop component to close the SideDrawer.

--- React/CompleteGuide/burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ React/CompleteGuide/burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -8,10 +8,18 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css';
 
 const sideDrawer = (props) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+
     return (
         <Aux>
-            <Backdrop show />
-            <div className={classes.SideDrawer} >
+            <Backdrop 
+                show={props.open} 
+                clicked={props.closed} />
+            <div className={attachedClasses.join(' ')}>
                 <div className={classes.Logo}>
                     <Logo />
                 </div>
