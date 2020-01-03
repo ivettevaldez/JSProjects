c2cbf9531a71219659a312a38222c634f910dfb1
Ivette Valdez <ivette.valdez.e@gmail.com>
2020-01-03 18:02:11 +0100

    ReactCompleteGuide: Updating state immutably.

--- React/CompleteGuide/redux-example/src/containers/Counter/Counter.js
+++ React/CompleteGuide/redux-example/src/containers/Counter/Counter.js
@@ -13,6 +13,17 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
                 <CounterControl label="Add 10" clicked={this.props.onAddCounter} />
                 <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter} />
+                <hr />
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map(result => (
+                        <li 
+                            key={result.id}
+                            onClick={this.props.onDeleteResult}>
+                                {result.value}
+                        </li>
+                    ))}
+                </ul>
             </div>
         );
     }
@@ -20,7 +31,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        storedResults: state.results
     }
 }
 
@@ -29,7 +41,9 @@ const mapDispatchToProps = dispatch => {
         onIncrementCounter: () => dispatch({type: "INCREMENT"}),
         onDecrementCounter: () => dispatch({type: "DECREMENT"}),
         onAddCounter: () => dispatch({type: "ADD", value: 10}),
-        onSubtractCounter: () => dispatch({type: "SUBTRACT", value: 15})
+        onSubtractCounter: () => dispatch({type: "SUBTRACT", value: 15}),
+        onStoreResult: () => dispatch({type: 'STORE_RESULT'}),
+        onDeleteResult: () => dispatch({type: 'DELETE_RESULT'})
     }
 }
 
